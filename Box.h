3dff2a7d523b920a16245d30650d6df37a371734
Shalini Muthumali Athukorala <99632841+IT21169076@users.noreply.github.com>
2022-03-27 23:09:03 +0530

    6 PART 2 EXE 3

--- Box.h
+++ Box.h
@@ -5,7 +5,12 @@ class Box {
        int height;
     public:
        // write prototypes of setters for length, width and height
+void setdetails (int len , int wid ,int hei);
        // write prototypes of getters for length, width and height
+int getHeight();
+int getLength ();
+int getWidth ();
+
  
        int calcVolume();
 };
