1823777f0e4e98e0deb87d9b260d36bc9050a9b9
Shalini Muthumali Athukorala <99632841+IT21169076@users.noreply.github.com>
2022-03-27 23:08:15 +0530

    6 PART EXE 2

--- Box.cpp
+++ Box.cpp
@@ -1,7 +1,25 @@
-#include "Box.h"
+#include "box.h"
 
 // Implement setters and getters
+void Box::setdetails (int len , int wid ,int hei){
+  length = len ;
+  width = wid ;
+  height = hei ;
+}
+int Box::getHeight(){
+return  length;
+}
+int Box::getLength(){
+return  width;
+  }
+int Box::getWidth(){
+return  height;
+  }
+
+
+
 
 // Implemenet the calcVolume() unction
 int Box::calcVolume() {
+  return length* width * height ;
 }
